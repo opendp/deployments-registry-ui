ca03a18e27154764e2ee5b8d68d55e3ec59178d3
Chuck McCallum <mccalluc@users.noreply.github.com>
2025-07-14 09:25:16 -0400

    Add playwright test (#20)
    * add CI; expect failure
    
    * fix test
    
    * stub python tests
    
    * update gitignore
    
    * copy playwright instructions from DP Wizard
    
    * trivial test of page load
    
    * more trivial tests
    
    * simplify: just use the static site
    
    * add an exclude
    
    * retain traces
    
    * recurse into submodules
    
    * tighten playwright assertion
    
    * Just use the Census epsilon to test load

---
+++ tests/test_ui.py
@@ -0,0 +1,31 @@
+from playwright.sync_api import Page, expect
+from pathlib import Path
+
+
+bp = "BREAKPOINT()".lower()
+if bp in Path(__file__).read_text():
+    raise Exception(
+        f"Instead of `{bp}`, use `page.pause()` in playwright tests. "
+        "See https://playwright.dev/python/docs/debug"
+        "#run-a-test-from-a-specific-breakpoint"
+    )
+
+
+def test_ui(page: Page):
+    # Page load?
+    root = Path(__file__).parent.parent
+    page.goto(f"file://{root}/_site/index.html")
+    expect(
+        page.get_by_role("heading", name="Differential Privacy Deployments Registry")
+    ).to_be_visible()
+
+    # Schema load?
+    expect(
+        page.get_by_text("The name of the entity publishing the data product.")
+    ).to_be_visible()
+
+    # Data load?
+    expect(
+        # US Census:
+        page.get_by_text("ε: 19.61")
+    ).to_be_visible()
